c20aa92a9bc8db849c20bba96c56f15bde04af42
Nathan Potter <natepotter95@gmail.com>
2024-04-09 07:04:39 -0700

    april 9

--- src/pages/UpcomingPage.js
+++ src/pages/UpcomingPage.js
@@ -3,15 +3,16 @@ import background from "../content/images/upcoming-page.png";
 
 const UpcomingPage = () => {
   return (
-    <div className="page">
-      <img className="upcoming-background-img" src={background} alt="Upcoming image" />
-      <div className="upcoming-heading">
-        <div>
+    <div>
+      <div className="upcoming-img">
+        {/* <img className="upcoming-background-img" src={background} alt="Upcoming image" /> */}
+        <div className="upcoming-header">
           <p className="text-overlay">Experience The 6202 Band</p>
         </div>
       </div>
-      <div>
-        <h1>Upcoming and Recent Events</h1>
+      <div className="upcoming-content">
+        <h1 className="upcoming-subheader">Upcoming and Recent Events</h1>
+        <div className="events-gallery"></div>
       </div>
     </div>
   );
