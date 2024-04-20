968cff137773aeb4f6a37d96669c5df1a2177ce3
Nathan Potter <natepotter95@gmail.com>
2024-04-20 09:01:39 -0700

    media gallery

--- src/content/galleryExport.js
+++ src/content/galleryExport.js
@@ -1,24 +1,24 @@
-import gallery1 from "../content/images/gallery/Band-full.png";
-import gallery2 from "../content/images/gallery/Bob-focus.png";
+import gallery1 from "../content/images/gallery/John-slaying-3.png";
+import gallery2 from "../content/images/gallery/Larry-sing-3.png";
 import gallery3 from "../content/images/gallery/Bob-focus2.png";
-import gallery4 from "../content/images/gallery/Bob-new.png";
-import gallery5 from "../content/images/gallery/John-Gunslinger.png";
-import gallery6 from "../content/images/gallery/Johnny.png";
-import gallery7 from "../content/images/gallery/john-slaying.png";
-import gallery8 from "../content/images/gallery/John-slaying-2.png";
-import gallery9 from "../content/images/gallery/John-slaying-3.png";
-import gallery10 from "../content/images/gallery/Larry-sing-2.png";
-import gallery11 from "../content/images/gallery/Larry-sing-3.png";
+import gallery4 from "../content/images/gallery/shane-2.png";
+import gallery5 from "../content/images/gallery/TD-Sing-2.png";
+import gallery6 from "../content/images/gallery/TD_Singing_2.png";
+import gallery7 from "../content/images/gallery/Shane-drums-2.png";
+import gallery8 from "../content/images/gallery/Larry-sing-2.png";
+import gallery9 from "../content/images/gallery/John-Gunslinger.png";
+import gallery10 from "../content/images/gallery/Bob-new.png";
+import gallery11 from "../content/images/gallery/john-slaying.png";
 import gallery12 from "../content/images/gallery/Larry-singing.png";
-import gallery13 from "../content/images/gallery/Larry-singing-at-Maddies.png";
-import gallery14 from "../content/images/gallery/Larry-solo.png";
-import gallery15 from "../content/images/gallery/Shane.png";
-import gallery16 from "../content/images/gallery/shane-2.png";
-import gallery17 from "../content/images/gallery/Shane-drums-2.png";
-import gallery18 from "../content/images/gallery/TD_Singing_2.png";
-import gallery19 from "../content/images/gallery/TD-Sing-2.png";
+import gallery13 from "../content/images/gallery/Larry-solo.png";
+import gallery14 from "../content/images/gallery/Band-full.png";
+import gallery15 from "../content/images/gallery/John-slaying-2.png";
+import gallery16 from "../content/images/gallery/Shane.png";
+import gallery17 from "../content/images/gallery/Bob-focus.png";
+import gallery18 from "../content/images/gallery/Larry-singing-at-Maddies.png";
+import gallery19 from "../content/images/gallery/Johnny.png";
 
-export {
+const galleryImages = [
   gallery1,
   gallery2,
   gallery3,
@@ -38,4 +38,6 @@ export {
   gallery17,
   gallery18,
   gallery19,
-};
+];
+
+export default galleryImages;
--- src/pages/MediaPage.js
+++ src/pages/MediaPage.js
@@ -1,112 +1,19 @@
 import React from "react";
-import PhotoAlbum from "react-photo-album";
-import * as gallery from "../content/galleryExport.js";
-import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
-// import photos from ".../content/images/gallery";
+import galleryImages from "../content/galleryExport";
 
-const photos = [
-  {
-    src: gallery.gallery1,
-    width: 1,
-    height: 1,
-  },
-  {
-    src: gallery.gallery2,
-    width: 1,
-    height: 1,
-  },
-  {
-    src: gallery.gallery3,
-    width: 3,
-    height: 4,
-  },
-  {
-    src: gallery.gallery4,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery5,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery6,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery7,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery8,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery9,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery10,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery11,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery12,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery13,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery14,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery15,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery16,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery17,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery18,
-    width: 3,
-    height: 5,
-  },
-  {
-    src: gallery.gallery19,
-    width: 3,
-    height: 5,
-  },
-];
-
-const Gallery = () => {
+function MediaPage() {
   return (
-    <div className="media-gallery">
-      <PhotoAlbum layout="masonry" photos={photos} spacing={80} />;
+    <div className="media-page">
+      <h1 className="title">
+        6202 <span>On Stage Moments</span>
+      </h1>
+      <div className="photo-gallery">
+        {galleryImages.map((image, index) => (
+          <img key={index} src={image} alt={`Gallery Image ${index + 1}`} />
+        ))}
+      </div>
     </div>
   );
-};
-export default Gallery;
+}
+
+export default MediaPage;
