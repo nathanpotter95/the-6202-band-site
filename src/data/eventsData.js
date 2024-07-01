4f9baa67059b06ff9cca71a19a0c654f07ad12a0
Nathan Potter <natepotter95@gmail.com>
2024-07-01 06:21:48 -0700

    update upcoming events

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,45 +3,6 @@ import { FrontierClubLogo } from "../content/contentExport";
 const events = [
   {
     id: 1,
-    title: "6202 Returns to Rock Bottom Brewery",
-    date: "Sat, Jun 08, 2024",
-    time: "6:00pm - 9:00pm",
-    location: "Rock Bottom Brewery",
-    address: "1505 Park Central Dr, Highlands Ranch, CO 80126, USA",
-    description: "6202 plays at Rock Bottom Brewery's Summer Music Series",
-    logo: "https://rockbottom.com/wp-content/uploads/2021/09/logo-red.png",
-    link: "https://rockbottom.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.1984968083602!2d-105.01012232345994!3d39.55513160762741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c838fffffffff%3A0xa93df11417552e5e!2sRock%20Bottom%20Restaurant%20%26%20Brewery!5e0!3m2!1sen!2sus!4v1713718275662!5m2!1sen!2sus",
-  },
-  {
-    id: 2,
-    title: "Greenfield's Welcome 6202 for a Friday Night Show",
-    date: "Fri, Jun 21, 2024",
-    time: "8:00pm - 12:00am",
-    location: "Greenfield's",
-    address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
-    description: "6202 makes a return visit to Greenfield's in Lakewood",
-    logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
-    link: "https://greenfieldsbar.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
-  },
-  {
-    id: 3,
-    title: "6202 Returns to Wrigley's for a Saturday Night Special",
-    date: "Sat, Jun 29, 2024",
-    time: "4:00pm - 8:00pm",
-    location: "Wrigley's Chicago Bar & Grill",
-    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
-    description: "6202 takes to the outdoor stage at Wrigley's Chicago Bar & Grill in Golden",
-    logo: "https://www.wrigleyschicagobar.net/wp-content/themes/wrigleys/images/wrigleys-chicago-bar-grill-logo.png",
-    link: "https://www.wrigleyschicagobar.net/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6137.870539734098!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1713718330857!5m2!1sen!2sus",
-  },
-  {
-    id: 4,
     title: "6202 Invades the Platte River Grill",
     date: "Fri, Jul 05, 2024",
     time: "6:00pm - 10:00pm",
@@ -55,7 +16,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6147.716785210904!2d-105.02402112345811!3d39.607872304470796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c81d2039fe33b%3A0xb64c232dfb5265c2!2sPlatte%20River%20Bar%20and%20Grill!5e0!3m2!1sen!2sus!4v1713718353636!5m2!1sen!2sus",
   },
   {
-    id: 5,
+    id: 2,
     title: "6202 Debuts at Bucket List Tavern",
     date: "Fri, Jul 19, 2024",
     time: "",
@@ -69,7 +30,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3082.944245545774!2d-104.88333782346527!3d39.40276741672657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c99f4be22f16b%3A0x8477ce7ceae3c068!2sBucket%20List%20Tavern!5e0!3m2!1sen!2sus!4v1713718378563!5m2!1sen!2sus",
   },
   {
-    id: 6,
+    id: 3,
     title: "6202 Hits Rock Bottom Again!",
     date: "Sat, Jul 27, 2024",
     time: "6:00pm - 9:00pm",
@@ -82,7 +43,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.1984968083602!2d-105.01012232345994!3d39.55513160762741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c838fffffffff%3A0xa93df11417552e5e!2sRock%20Bottom%20Restaurant%20%26%20Brewery!5e0!3m2!1sen!2sus!4v1713718275662!5m2!1sen!2sus",
   },
   {
-    id: 7,
+    id: 4,
     title: "6202 Returns to Sheabeen Irish Pub",
     date: "Fri, Aug 16, 2024",
     time: "9:00pm - 12:00am",
@@ -95,7 +56,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.9045608600677!2d-104.80991162345566!3d39.67436140048686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c882f44a351eb%3A0xc41b87662b374cb9!2sSheabeens%20Irish%20Pub!5e0!3m2!1sen!2sus!4v1713718050501!5m2!1sen!2sus",
   },
   {
-    id: 8,
+    id: 5,
     title: "6202 Rocks the Palmer Lake Pub!",
     date: "Sat, Aug 31, 2024",
     time: "4:00pm - 7:00pm",
@@ -109,7 +70,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3096.357357865216!2d-104.8794648234758!3d39.098333534822146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2",
   },
   {
-    id: 9,
+    id: 6,
     title: "6202 Invades Greenfield's!",
     date: "Sat, Sep 07, 2024",
     time: "8:00pm - 12:00am",
@@ -122,20 +83,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
   },
   {
-    id: 10,
-    title: "6202 Plays the Outdoor Stage at the Platte River Grill",
-    date: "Fri, Sep 13, 2024",
-    time: "6:00pm - 10:00pm",
-    location: "The Platte",
-    address: "5995 S Santa Fe Dr, Littleton, CO 80120, USA",
-    description: "6202 makes a return visit to The Platte for another Friday night show.",
-    logo: "https://static.wixstatic.com/media/6f0583_fe575d3ea44a4511aa0ede5333f9ddd5~mv2.png/v1/fill/w_133,h_73,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Platte-River-Grill-logo_black.png",
-    link: "https://www.theplatteriverbarandgrill.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6147.716785210904!2d-105.02402112345811!3d39.607872304470796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c81d2039fe33b%3A0xb64c232dfb5265c2!2sPlatte%20River%20Bar%20and%20Grill!5e0!3m2!1sen!2sus!4v1713718353636!5m2!1sen!2sus",
-  },
-  {
-    id: 11,
+    id: 7,
     title: "6202 Returns to Wrigley's for a Saturday Night Special",
     date: "Sat, Sep 21, 2024",
     time: "4:00pm - 8:00pm",
@@ -148,7 +96,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6137.870539734098!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1713718330857!5m2!1sen!2sus",
   },
   {
-    id: 12,
+    id: 8,
     title: "6202 Makes a Return to the Stage at Takoda Tavern",
     date: "Fri, Oct 25, 2024",
     time: "7:30pm - 11:30pm",
@@ -162,7 +110,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
   },
   {
-    id: 13,
+    id: 9,
     title: "6202 Rocks the House at Takoda Tavern",
     date: "Fri, Dec 06, 2024",
     time: "7:30pm - 11:30pm",
@@ -176,20 +124,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
   },
   {
-    id: 14,
-    title: "6202 Plays Their Last 2024 Show at Greenfield's",
-    date: "Fri, Dec 20, 2024",
-    time: "8:00pm - 12:00am",
-    location: "Greenfield's",
-    address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
-    description: "6202 closes out 2024 with a Friday night show at Greenfield's",
-    logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
-    link: "https://greenfieldsbar.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
-  },
-  {
-    id: 15,
+    id: 10,
     title: "6202 Returns to The Frontier Club",
     date: "Sat, Nov 2, 2024",
     time: "8:00pm - 12:00am",
@@ -200,6 +135,19 @@ const events = [
     link: "https://www.thefrontierclubaurora.com/",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
+  },
+  {
+    id: 11,
+    title: "6202 Plays Their Last 2024 Show at Greenfield's",
+    date: "Fri, Dec 20, 2024",
+    time: "8:00pm - 12:00am",
+    location: "Greenfield's",
+    address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
+    description: "6202 closes out 2024 with a Friday night show at Greenfield's",
+    logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
+    link: "https://greenfieldsbar.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
   },
 ];
 
