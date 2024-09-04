ee6a2fc241b40c92457c21f569ca15c68e074040
Nathan Potter <natepotter95@gmail.com>
2024-09-04 06:45:24 -0700

    update events data

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,74 +3,6 @@ import { FrontierClubLogo } from "../content/contentExport";
 const events = [
   {
     id: 1,
-    title: "6202 Debuts at Bucket List Tavern",
-    date: "Fri, Jul 19, 2024",
-    time: "",
-    location: "Bucket List Tavern",
-    address: "3982 Limelight Ave Ste A, Castle Rock, CO 80109, USA",
-    description:
-      "6202 makes their first visit and plays the patio at Bucket List Tavern in Castle Rock.",
-    logo: "https://images.squarespace-cdn.com/content/v1/632239fbc9ab8874b248588d/15d47f55-e4ba-4c68-98d8-f275d79b2dcf/Bucketlist+Logo+72ppi+for+Black+Background.png?format=1500w",
-    link: "https://www.bucketlisttavern.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3082.944245545774!2d-104.88333782346527!3d39.40276741672657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c99f4be22f16b%3A0x8477ce7ceae3c068!2sBucket%20List%20Tavern!5e0!3m2!1sen!2sus!4v1713718378563!5m2!1sen!2sus",
-  },
-  {
-    id: 2,
-    title: "6202 Hits Rock Bottom Again!",
-    date: "Sat, Jul 27, 2024",
-    time: "6:00pm - 9:00pm",
-    location: "Rock Bottom Brewery",
-    address: "1505 Park Central Dr, Highlands Ranch, CO 80126, USA",
-    description: "6202 plays a second show for Rock Bottom Brewery's Summer Music Series.",
-    logo: "https://rockbottom.com/wp-content/uploads/2021/09/logo-red.png",
-    link: "https://rockbottom.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3076.1984968083602!2d-105.01012232345994!3d39.55513160762741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c838fffffffff%3A0xa93df11417552e5e!2sRock%20Bottom%20Restaurant%20%26%20Brewery!5e0!3m2!1sen!2sus!4v1713718275662!5m2!1sen!2sus",
-  },
-  {
-    id: 3,
-    title: "6202 Returns to Sheabeen Irish Pub",
-    date: "Fri, Aug 16, 2024",
-    time: "9:00pm - 12:00am",
-    location: "Sheabeen Irish Pub",
-    address: "2300 S Chambers Rd # A, Aurora, CO 80014, USA",
-    description: "6202 makes a return visit to Sheabeen Irish Pub for a Friday night show.",
-    logo: "https://www.sheabeenirishpub.com/images/logo2.jpg",
-    link: "https://www.sheabeenirishpub.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.9045608600677!2d-104.80991162345566!3d39.67436140048686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c882f44a351eb%3A0xc41b87662b374cb9!2sSheabeens%20Irish%20Pub!5e0!3m2!1sen!2sus!4v1713718050501!5m2!1sen!2sus",
-  },
-  {
-    id: 4,
-    title: "6202 Heading to Wrigley's for a Sunday Afternoon Show!",
-    date: "Sun, Aug 18, 2024",
-    time: "3:00pm - 7:00pm",
-    location: "Wrigley's Chicago Bar & Grill",
-    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
-    description:
-      "Ejoy the last days of summer! Come on over to Wrigley's to see 6202 perform a late afternoon show on their outdoor stage.",
-    logo: "https://www.wrigleyschicagobar.net/wp-content/themes/wrigleys/images/wrigleys-chicago-bar-grill-logo.png",
-    link: "https://www.wrigleyschicagobar.net/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.935269867049!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1720399962184!5m2!1sen!2sus",
-  },
-  {
-    id: 5,
-    title: "6202 Rocks the Palmer Lake Pub!",
-    date: "Sat, Aug 31, 2024",
-    time: "4:00pm - 7:00pm",
-    location: "Palmer Lake Pub",
-    address: "876 CO-105, Palmer Lake, CO 80133, USA",
-    description:
-      "6202 plays a parking lot party at Palmer Lake Pub in beautiful Monument. Come on out for a great late afternoon of Rock music and enjoy the amazing food, drinks and service at Palmer Lake Pub. Chantel and her talented staff will take care of the food and drinks, while 6202 brings the heat!!",
-    logo: "https://static.wixstatic.com/media/e903ec_907eba549830449a9eb9603790844e96~mv2.jpg/v1/crop/x_2,y_0,w_957,h_838/fill/w_317,h_274,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/PLP_Logo_White.jpg",
-    link: "https://www.palmerlakepub.net/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3096.357357865216!2d-104.8794648234758!3d39.098333534822146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2",
-  },
-  {
-    id: 6,
     title: "6202 Invades Greenfield's!",
     date: "Sat, Sep 07, 2024",
     time: "8:00pm - 12:00am",
@@ -83,7 +15,20 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
   },
   {
-    id: 7,
+    id: 2,
+    title: "6202 Brings the Hype to Hoffbrau!",
+    date: "Thurs, Oct 03, 2024",
+    time: "9:00pm - 12:00am",
+    location: "Hoffbrau Bar & Grill",
+    address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
+    description: "Come on out to Hoffbrau Bar & Grill for a 6202 Thursday night special.",
+    logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
+    link: "https://www.hoffbrau.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
+  },
+  {
+    id: 3,
     title: "6202 Returns to Wrigley's for a Saturday Night Special",
     date: "Sat, Sep 21, 2024",
     time: "4:00pm - 8:00pm",
@@ -96,7 +41,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.935269867049!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1720399962184!5m2!1sen!2sus",
   },
   {
-    id: 8,
+    id: 4,
     title: "6202 Makes a Return to the Stage at Takoda Tavern",
     date: "Fri, Oct 25, 2024",
     time: "7:30pm - 11:30pm",
@@ -110,7 +55,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
   },
   {
-    id: 9,
+    id: 5,
     title: "6202 Returns to The Frontier Club",
     date: "Sat, Nov 2, 2024",
     time: "8:00pm - 12:00am",
@@ -123,7 +68,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
   },
   {
-    id: 10,
+    id: 6,
     title: "6202 Rocks the House at Takoda Tavern",
     date: "Fri, Dec 06, 2024",
     time: "7:30pm - 11:30pm",
@@ -137,7 +82,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
   },
   {
-    id: 11,
+    id: 7,
     title: "6202 Plays Their Last 2024 Show at Greenfield's",
     date: "Fri, Dec 20, 2024",
     time: "8:00pm - 12:00am",
