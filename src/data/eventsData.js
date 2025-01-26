7fddd9b218e22f49128d24cfbb40fc505ed88f77
Nathan Potter <natepotter95@gmail.com>
2025-01-26 14:42:59 -0800

    update events

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,48 +3,22 @@ import { FrontierClubLogo } from "../content/contentExport";
 const events = [
   {
     id: 1,
-    title: "6202 Returns to Wrigley's for a Saturday Night Special",
-    date: "Sat, Sep 21, 2024",
-    time: "3:00pm - 7:00pm",
-    location: "Wrigley's Chicago Bar & Grill",
-    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
-    description: "6202 is back at Wrigley's for a Fall show on the outdoor stage",
-    logo: "https://www.wrigleyschicagobar.net/wp-content/themes/wrigleys/images/wrigleys-chicago-bar-grill-logo.png",
-    link: "https://www.wrigleyschicagobar.net/",
+    title: "Rockin' Into February at Greenfields with 6202",
+    date: "Sat, Feb 8, 2025",
+    time: "8:00pm - 12:00am",
+    location: "Greenfield's",
+    address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
+    description:
+      "Start your month off right with a night of rock music! Join us at Greenfields for the ultimate experience.",
+    logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
+    link: "https://greenfieldsbar.com/",
     mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.935269867049!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1720399962184!5m2!1sen!2sus",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
   },
   {
     id: 2,
-    title: "6202 Brings the Hype to Hoffbrau!",
-    date: "Thurs, Oct 03, 2024",
-    time: "8:30pm - 11:30pm",
-    location: "Hoffbrau Bar & Grill",
-    address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
-    description: "Come on out to Hoffbrau Bar & Grill for a 6202 Thursday night special.",
-    logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
-    link: "https://www.hoffbrau.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
-  },
-  {
-    id: 4,
-    title: "6202 Makes a Return to the Stage at Takoda Tavern",
-    date: "Fri, Oct 25, 2024",
-    time: "7:30pm - 11:30pm",
-    location: "Takoda Tavern",
-    address: "12365 Pine Bluffs Way, Parker, CO 80134, USA",
-    description:
-      "6202 takes to the stage for a Friday night show. Don't miss everything Takoda has to offer; outstanding food, a huge variety of drink options, and of course, great music with 6202.",
-    logo: "http://nebula.wsimg.com/61ee8aaed0b74e6676d11f86c8ae73eb?AccessKeyId=F95A2FBD5F21156E5012&disposition=0&alloworigin=1",
-    link: "http://www.takodatavern.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
-  },
-  {
-    id: 5,
     title: "6202 Returns to The Frontier Club",
-    date: "Sat, Nov 2, 2024",
+    date: "Sat, Mar 1, 2025",
     time: "8:00pm - 12:00am",
     location: "The Frontier Club",
     address: "18881 E Colfax Ave #4, Aurora, CO 80011, USA",
@@ -55,46 +29,183 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
   },
   {
-    id: 6,
-    title: "6202 Back for Another Epic Night at Hoffbrau!",
-    date: "Thurs, Nov 14, 2024",
+    id: 3,
+    title: "6202 Brings the Hype to Hoffbrau!",
+    date: "Thurs, Mar 20, 2025",
     time: "8:30pm - 11:30pm",
     location: "Hoffbrau Bar & Grill",
     address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
-    description:
-      "Join us for round two as 6202 brings more energy and excitement to Hoffbrau Bar & Grill for a second special Thursday night event.",
+    description: "Come kick off spring at Hoffbrau Bar & Grill for a 6202 Thursday night special.",
     logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
     link: "https://www.hoffbrau.com/",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
   },
   {
-    id: 7,
+    id: 4,
+    title: "Spring Rockin' at Hoffbrau",
+    date: "Thurs, May 29, 2025",
+    time: "8:30pm - 11:30pm",
+    location: "Hoffbrau Bar & Grill",
+    address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
+    description:
+      "As the weather warms, we’ll be turning up the heat with rock hits from every era at Hoffbrau! Perfect for your Thursday night out.",
+    logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
+    link: "https://www.hoffbrau.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
+  },
+  {
+    id: 5,
+    title: "Rock Out at Palmer Lake Pub",
+    date: "Sat, Jun 14, 2025",
+    time: "4:00pm - 7:30pm",
+    location: "Palmer Lake Pub",
+    address: "876 CO-105, Palmer Lake, CO 80133, USA",
+    description:
+      "Enjoy a sunny Saturday afternoon of rock classics and modern hits at Palmer Lake Pub! It’s the perfect setting for some fun.",
+    logo: "https://static.wixstatic.com/media/e903ec_907eba549830449a9eb9603790844e96~mv2.jpg/v1/crop/x_2,y_0,w_957,h_838/fill/w_317,h_274,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/PLP_Logo_White.jpg",
+    link: "https://www.palmerlakepub.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198658.7515448148!2d-104.91836088769759!3d38.92299481431714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2sus!4v1737930499877!5m2!1sen!2sus",
+  },
+  {
+    id: 6,
     title: "6202 Rocks the House at Takoda Tavern",
-    date: "Fri, Dec 06, 2024",
+    date: "Fri, Jun 27, 2025",
     time: "7:30pm - 11:30pm",
     location: "Takoda Tavern",
     address: "12365 Pine Bluffs Way, Parker, CO 80134, USA",
     description:
-      "6202 makes its last 2024 visit to Takoda Tavern with a Friday night in December to remember!",
+      "Kick off the weekend with the best rock hits from the ‘60s to today! Join us at Takoda Tavern for a high-energy night.",
     logo: "http://nebula.wsimg.com/61ee8aaed0b74e6676d11f86c8ae73eb?AccessKeyId=F95A2FBD5F21156E5012&disposition=0&alloworigin=1",
     link: "http://www.takodatavern.com/",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
   },
   {
+    id: 7,
+    title: "Summer Rock Vibes at Palmer Lake Pub",
+    date: "Sat, Aug 16, 2025",
+    time: "4:00pm - 7:30pm",
+    location: "Palmer Lake Pub",
+    address: "876 CO-105, Palmer Lake, CO 80133, USA",
+    description:
+      "Rock out this summer at Palmer Lake Pub with all your favorite songs! It’s the perfect mix of good tunes and good vibes.",
+    logo: "https://static.wixstatic.com/media/e903ec_907eba549830449a9eb9603790844e96~mv2.jpg/v1/crop/x_2,y_0,w_957,h_838/fill/w_317,h_274,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/PLP_Logo_White.jpg",
+    link: "https://www.palmerlakepub.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198658.7515448148!2d-104.91836088769759!3d38.92299481431714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2sus!4v1737930499877!5m2!1sen!2sus",
+  },
+  {
     id: 8,
-    title: "6202 Plays Their Last 2024 Show at Greenfield's",
-    date: "Fri, Dec 20, 2024",
+    title: "Fall Rock Frenzy at Greenfields",
+    date: "Fri, Oct 3, 2025",
     time: "8:00pm - 12:00am",
     location: "Greenfield's",
     address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
-    description: "6202 closes out 2024 with a Friday night show at Greenfield's",
+    description:
+      "Get ready for a fall rock frenzy at Greenfields! We’re bringing the classic hits and modern rock sounds for an unforgettable night.",
     logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
     link: "https://greenfieldsbar.com/",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
   },
+
+  // {
+  //   id: 1,
+  //   title: "6202 Returns to Wrigley's for a Saturday Night Special",
+  //   date: "Sat, Sep 21, 2024",
+  //   time: "3:00pm - 7:00pm",
+  //   location: "Wrigley's Chicago Bar & Grill",
+  //   address: "18200 W Colfax Ave, Golden, CO 80401, USA",
+  //   description: "6202 is back at Wrigley's for a Fall show on the outdoor stage",
+  //   logo: "https://www.wrigleyschicagobar.net/wp-content/themes/wrigleys/images/wrigleys-chicago-bar-grill-logo.png",
+  //   link: "https://www.wrigleyschicagobar.net/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.935269867049!2d-105.2077428234542!3d39.718637397830946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley&#39;s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1720399962184!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 2,
+  //   title: "6202 Brings the Hype to Hoffbrau!",
+  //   date: "Thurs, Oct 03, 2024",
+  //   time: "8:30pm - 11:30pm",
+  //   location: "Hoffbrau Bar & Grill",
+  //   address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
+  //   description: "Come on out to Hoffbrau Bar & Grill for a 6202 Thursday night special.",
+  //   logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
+  //   link: "https://www.hoffbrau.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 4,
+  //   title: "6202 Makes a Return to the Stage at Takoda Tavern",
+  //   date: "Fri, Oct 25, 2024",
+  //   time: "7:30pm - 11:30pm",
+  //   location: "Takoda Tavern",
+  //   address: "12365 Pine Bluffs Way, Parker, CO 80134, USA",
+  //   description:
+  //     "6202 takes to the stage for a Friday night show. Don't miss everything Takoda has to offer; outstanding food, a huge variety of drink options, and of course, great music with 6202.",
+  //   logo: "http://nebula.wsimg.com/61ee8aaed0b74e6676d11f86c8ae73eb?AccessKeyId=F95A2FBD5F21156E5012&disposition=0&alloworigin=1",
+  //   link: "http://www.takodatavern.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 5,
+  //   title: "6202 Returns to The Frontier Club",
+  //   date: "Sat, Nov 2, 2024",
+  //   time: "8:00pm - 12:00am",
+  //   location: "The Frontier Club",
+  //   address: "18881 E Colfax Ave #4, Aurora, CO 80011, USA",
+  //   description: "6202 Makes a Return Visit to The Frontier Club",
+  //   logo: FrontierClubLogo,
+  //   link: "https://www.thefrontierclubaurora.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 6,
+  //   title: "6202 Back for Another Epic Night at Hoffbrau!",
+  //   date: "Thurs, Nov 14, 2024",
+  //   time: "8:30pm - 11:30pm",
+  //   location: "Hoffbrau Bar & Grill",
+  //   address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
+  //   description:
+  //     "Join us for round two as 6202 brings more energy and excitement to Hoffbrau Bar & Grill for a second special Thursday night event.",
+  //   logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
+  //   link: "https://www.hoffbrau.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 7,
+  //   title: "6202 Rocks the House at Takoda Tavern",
+  //   date: "Fri, Dec 06, 2024",
+  //   time: "7:30pm - 11:30pm",
+  //   location: "Takoda Tavern",
+  //   address: "12365 Pine Bluffs Way, Parker, CO 80134, USA",
+  //   description:
+  //     "6202 makes its last 2024 visit to Takoda Tavern with a Friday night in December to remember!",
+  //   logo: "http://nebula.wsimg.com/61ee8aaed0b74e6676d11f86c8ae73eb?AccessKeyId=F95A2FBD5F21156E5012&disposition=0&alloworigin=1",
+  //   link: "http://www.takodatavern.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
+  // },
+  // {
+  //   id: 8,
+  //   title: "6202 Plays Their Last 2024 Show at Greenfield's",
+  //   date: "Fri, Dec 20, 2024",
+  //   time: "8:00pm - 12:00am",
+  //   location: "Greenfield's",
+  //   address: "3355 S Yarrow St E101, Lakewood, CO 80227, USA",
+  //   description: "6202 closes out 2024 with a Friday night show at Greenfield's",
+  //   logo: "https://img1.wsimg.com/isteam/ip/0ca600d5-a580-40b5-ad95-b7c4b4f7b18f/4442d3a6-6c6a-47fb-a48b-fe73880694f4.png/:/rs=w:159,h:106,cg:true,m/cr=w:159,h:106/qt=q:100/ll",
+  //   link: "https://greenfieldsbar.com/",
+  //   mapEmbed:
+  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6143.56885764504!2d-105.08625812345643!3d39.654565801673684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b81d20db6f029%3A0x57debcf670f6b798!2sGreenfields%20Pool%20%26%20Sports%20Bar!5e0!3m2!1sen!2sus!4v1713718305493!5m2!1sen!2sus",
+  // },
 ];
 
 export default events;
