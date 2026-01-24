d7b83c0b9f0cb4c164793ef2f3b57ac5fb36a5f2
Nate-Potter <natepotter95@gmail.com>
2026-01-24 09:31:05 -0800

    update events, ran build

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,34 +3,6 @@ import { FrontierClubLogo } from "../content/contentExport";
 const events = [
   {
     id: 1,
-    title: "6202 Rocks the House at Takoda Tavern",
-    date: "Fri, Nov 28, 2025",
-    time: "7:30pm - 11:30pm",
-    location: "Takoda Tavern",
-    address: "12365 Pine Bluffs Way, Parker, CO 80134, USA",
-    description:
-      "6202 makes another visit to Takoda Tavern with a Friday night in November to remember!",
-    logo: "http://nebula.wsimg.com/61ee8aaed0b74e6676d11f86c8ae73eb?AccessKeyId=F95A2FBD5F21156E5012&disposition=0&alloworigin=1",
-    link: "http://www.takodatavern.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
-  },
-  {
-    id: 2,
-    title: "A Thursday Night with 6202 at Hoffbrau",
-    date: "Thurs, Dec 11, 2025",
-    time: "8:30pm - 11:30pm",
-    location: "Hoffbrau Bar & Grill",
-    address: "9110 Wadsworth Pkwy, Westminster, CO 80021, USA",
-    description:
-      "Warm up your Thursday night with 6202 bringing your favorite hits to Hoffbrau Bar & Grill. Great music, good friends, and festive vibes all around.",
-    logo: "https://www.hoffbrau.com/sites/all/themes/bestmobile/logo.png",
-    link: "https://www.hoffbrau.com/",
-    mapEmbed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4331.127240933925!2d-105.08243352872017!3d39.861480918289885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c78cd4a7a2535%3A0xc28a7f4087eb002d!2sHoffbrau!5e0!3m2!1sen!2sus!4v1725456794747!5m2!1sen!2sus",
-  },
-  {
-    id: 3,
     title: "6202 Live at Misfits: A Winter Night of Rock",
     date: "Sat, Jan 24, 2026",
     time: "7:00pm - 11:00pm",
@@ -44,7 +16,7 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.962065114134!2d-105.12926768792789!3d39.74050297143766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84201aedd587%3A0x2eb10195fc69d76b!2sMisfits%20Sports%20Bar%20And%20Grill!5e0!3m2!1sen!2sus!4v1764014717988!5m2!1sen!2sus",
   },
   {
-    id: 4,
+    id: 2,
     title: "6202 Live at Takoda: Saturday Night Soundtrack",
     date: "Sat, Feb 28, 2026",
     time: "7:30pm - 11:30pm",
@@ -56,6 +28,34 @@ const events = [
     link: "http://www.takodatavern.com/",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.008369609031!2d-104.75976522346205!3d39.49172561141725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c91865e8c9181%3A0x4b6ef77cfb153c42!2sTakoda%20Tavern!5e0!3m2!1sen!2sus!4v1713718792379!5m2!1sen!2sus",
+  },
+  {
+    id: 3,
+    title: "6202 Live at Sheabeen Irish Pub – Friday Night Kickoff",
+    date: "Fri, Mar 27, 2026",
+    time: "7:30pm - 11:30pm",
+    location: "Sheabeen Irish Pub",
+    address: "2300 S Chambers Rd # A, Aurora, CO 80014, USA",
+    description:
+      "Kick off your Friday night with 6202 at Sheabeen Irish Pub. Expect high-energy live music, great drinks, and the perfect start to the weekend.",
+    logo: "https://www.sheabeenirishpub.com/images/logo2.jpg",
+    link: "https://www.sheabeenirishpub.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.9045608600677!2d-104.80991162345566!3d39.67436140048686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c882f44a351eb%3A0xc41b87662b374cb9!2sSheabeens%20Irish%20Pub!5e0!3m2!1sen!2sus!4v1713718050501!5m2!1sen!2sus",
+  },
+  {
+    id: 4,
+    title: "6202 Returns to Sheabeen – Spring Friday Party",
+    date: "Fri, May 8, 2026",
+    time: "7:30pm - 11:30pm",
+    location: "Sheabeen Irish Pub",
+    address: "2300 S Chambers Rd # A, Aurora, CO 80014, USA",
+    description:
+      "6202 returns to Sheabeen Irish Pub for another packed Friday night of live music, familiar favorites, and nonstop fun.",
+    logo: "https://www.sheabeenirishpub.com/images/logo2.jpg",
+    link: "https://www.sheabeenirishpub.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.9045608600677!2d-104.80991162345566!3d39.67436140048686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c882f44a351eb%3A0xc41b87662b374cb9!2sSheabeens%20Irish%20Pub!5e0!3m2!1sen!2sus!4v1713718050501!5m2!1sen!2sus",
   },
   {
     id: 5,
@@ -73,6 +73,21 @@ const events = [
   },
   {
     id: 6,
+    title: "6202 Live at Frontier Club – Late Night Throwdown",
+    date: "Sat, May 30, 2026",
+    time: "8:00pm - 12:00am",
+    location: "The Frontier Club",
+    address: "18881 E Colfax Ave #4, Aurora, CO 80011, USA",
+    description:
+      "Frontier Club turns up as 6202 delivers a full-throttle late-night set. Grab a drink, hit the dance floor, and close out your Saturday right.",
+    logo: FrontierClubLogo,
+    link: "https://www.thefrontierclubaurora.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
+  },
+
+  {
+    id: 7,
     title: "6202 Summer Starter at Palmer Lake Pub",
     date: "Sat, Jun 13, 2026",
     time: "4:00pm - 7:00pm",
@@ -86,7 +101,35 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198658.7515448148!2d-104.91836088769759!3d38.92299481431714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2sus!4v1737930499877!5m2!1sen!2sus",
   },
   {
-    id: 7,
+    id: 8,
+    title: "6202 Live at Wrigley's – Summer Patio Show",
+    date: "Sat, Jun 20, 2026",
+    time: "4:00pm - 8:00pm",
+    location: "Wrigley's Chicago Bar & Grill",
+    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
+    description:
+      "Join 6202 for a perfect summer afternoon at Wrigley’s. Live music, cold drinks, and great vibes on the outdoor stage.",
+    logo: "https://static.spotapps.co/website_images/ab_websites/86277_website_v2/logo_new.png",
+    link: "https://www.wrigleyschicagobar.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7649.01668374426!2d-105.20194418293227!3d39.71689675468152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley%27s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1769273472351!5m2!1sen!2sus",
+  },
+  {
+    id: 9,
+    title: "6202 Returns to Frontier Club – Mid-Summer Bash",
+    date: "Sat, Jul 18, 2026",
+    time: "8:00pm - 12:00am",
+    location: "The Frontier Club",
+    address: "18881 E Colfax Ave #4, Aurora, CO 80011, USA",
+    description:
+      "Summer heats up as 6202 returns to Frontier Club for another big Saturday night of live music and crowd favorites.",
+    logo: FrontierClubLogo,
+    link: "https://www.thefrontierclubaurora.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
+  },
+  {
+    id: 10,
     title: "6202 Mid-Summer Session at Palmer Lake",
     date: "Sat, Aug 1, 2026",
     time: "4:00pm - 7:00pm",
@@ -100,7 +143,21 @@ const events = [
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d198658.7515448148!2d-104.91836088769759!3d38.92299481431714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876cad0616815a73%3A0x1c8a413e97c2c0fd!2sPalmer%20Lake%20Pub!5e0!3m2!1sen!2sus!4v1737930499877!5m2!1sen!2sus",
   },
   {
-    id: 8,
+    id: 11,
+    title: "6202 Live at Wrigley's – Late Summer Session",
+    date: "Sat, Aug 15, 2026",
+    time: "4:00pm - 8:00pm",
+    location: "Wrigley's Chicago Bar & Grill",
+    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
+    description:
+      "Spend your Saturday afternoon with 6202 at Wrigley’s. A relaxed outdoor show packed with great music and summer energy.",
+    logo: "https://static.spotapps.co/website_images/ab_websites/86277_website_v2/logo_new.png",
+    link: "https://www.wrigleyschicagobar.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7649.01668374426!2d-105.20194418293227!3d39.71689675468152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley%27s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1769273472351!5m2!1sen!2sus",
+  },
+  {
+    id: 12,
     title: "Another 6202 Evening at The Angry Clover",
     date: "Fri, Aug 28, 2026",
     time: "8:00pm - 12:00am",
@@ -112,6 +169,48 @@ const events = [
     link: "https://theangryclover.com",
     mapEmbed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3072.8006158939647!2d-104.81220828793235!3d39.631692971457994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c88faece6afb3%3A0x1a2c9ed4ddc2af20!2sThe%20Angry%20Clover!5e0!3m2!1sen!2sus!4v1764014890870!5m2!1sen!2sus",
+  },
+  {
+    id: 13,
+    title: "6202 Live at Wrigley's – Sunday Funday Show",
+    date: "Sun, Sep 13, 2026",
+    time: "3:00pm - 7:00pm",
+    location: "Wrigley's Chicago Bar & Grill",
+    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
+    description:
+      "Wrap up your weekend with 6202 at Wrigley’s. A laid-back Sunday show featuring great tunes, drinks, and mountain views.",
+    logo: "https://static.spotapps.co/website_images/ab_websites/86277_website_v2/logo_new.png",
+    link: "https://www.wrigleyschicagobar.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7649.01668374426!2d-105.20194418293227!3d39.71689675468152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley%27s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1769273472351!5m2!1sen!2sus",
+  },
+  {
+    id: 14,
+    title: "6202 Returns to Frontier Club – Fall Night Rock Show",
+    date: "Sat, Sep 19, 2026",
+    time: "8:00pm - 12:00am",
+    location: "The Frontier Club",
+    address: "18881 E Colfax Ave #4, Aurora, CO 80011, USA",
+    description:
+      "Fall nights hit different at Frontier Club. Catch 6202 bringing the energy with a stacked setlist and a packed dance floor.",
+    logo: FrontierClubLogo,
+    link: "https://www.thefrontierclubaurora.com/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29193.73561161145!2d-104.76297589026187!3d39.72586896213633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c63c40dc2af9d%3A0x294d72ce21dd2ae2!2sThe%20Frontier%20Club!5e0!3m2!1sen!2sus!4v1716481108592!5m2!1sen!2sus",
+  },
+  {
+    id: 15,
+    title: "6202 Live at Wrigley's – Fall Patio Show",
+    date: "Sat, Oct 17, 2026",
+    time: "4:00pm - 8:00pm",
+    location: "Wrigley's Chicago Bar & Grill",
+    address: "18200 W Colfax Ave, Golden, CO 80401, USA",
+    description:
+      "Celebrate fall with 6202 at Wrigley’s. An afternoon outdoor show filled with crowd favorites and crisp Colorado air.",
+    logo: "https://static.spotapps.co/website_images/ab_websites/86277_website_v2/logo_new.png",
+    link: "https://www.wrigleyschicagobar.net/",
+    mapEmbed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7649.01668374426!2d-105.20194418293227!3d39.71689675468152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b84a526ece61f%3A0xe7a24f1d4def9111!2sWrigley%27s%20Chicago%20Bar%20%26%20Grill!5e0!3m2!1sen!2sus!4v1769273472351!5m2!1sen!2sus",
   },
 ];
 
